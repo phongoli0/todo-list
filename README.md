94694d4b708e426334c7524cbc0029cbe9b5713d
phongoli0 <phongoli0@gmail.com>
2019-09-26 02:55:52 +0000

    my first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Phong's todo list
+
+this is how to keep track of what i am doing
+
+
---
+++ todo.js
@@ -0,0 +1,29 @@
+
+function app() {
+
+pjs.defineDisplay("display", "todo.json");
+
+var list = pjs.query("SELECT * FROM todo")
+
+display.grid.replaceRecords(list);
+
+while(true){
+display.todoScreen.execute();
+  if (add) addItem(display.grid, newItem);
+  removeItems(entry => !entry.remove);
+  }
+
+}
+
+function addItem(grid, newItem){
+  grid.push({item: newItem});
+  pjs.query("INSERT INTO todo SET ?", {item: newItem})
+}
+
+function removeItems(grid){
+  var recordsToRemove = grid.filter(entry => entry.remove);
+  recordsToRemove.forEach(record => pjs.query("DELETE FROM todo WHERE item = ?", record.item));
+  grid.applyFilter(entry => !entry.remove);
+}
+
+exports.default = app;
